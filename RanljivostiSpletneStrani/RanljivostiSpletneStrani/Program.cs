ff6d030e1d65c6110350770da8ca9ba42ada580a
YourGitHubUsername <YourGitHubEmail>
2026-07-11 20:03:53 +0200

    Dodane C# WPF naloge iz varnosti z opisi

---
+++ RanljivostiSpletneStrani/RanljivostiSpletneStrani/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RanljivostiSpletneStrani.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=baza.db"));
+
+
+builder.Services.AddDistributedMemoryCache(); // Potrebno za shranjevanje sej v RAM
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20); // Seja poteče po 20 min
+    options.Cookie.HttpOnly = true; // Piškotek ni dostopen preko JS (varnost!)
+    options.Cookie.IsEssential = true; // Nujno za delovanje strani
+});
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.EnsureCreated();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseSession(); 
+
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
